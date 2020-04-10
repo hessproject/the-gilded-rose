127c478966385ac7604733af8efb68c3ee0e8eb7
Nick Hess <hessproject@gmail.com>
2020-04-10 16:07:25 -0700

    Add comments, remove outstanding statement for sellIn < 0

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -26,10 +26,12 @@ class GildedRose {
         for(int days = 0; days < numberOfDays; days++){
             for (int i = 0; i < items.length; i++) {
 
+                //Legendary items don't need to be adjusted, go to next loop iteration
                 if(legendaryItems.contains(items[i].name)){
                     continue;
                 }
 
+                //Degrading process for perishable items
                 if (!nonDegradables.contains(items[i].name)) {
                     if (items[i].quality > 0) {
                         if(items[i].sellIn >= 0){
@@ -39,20 +41,19 @@ class GildedRose {
                         }
                     }
                 } else {
+
+                    //Special case for concert tickets
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         items[i] = adjustConcertTickets(items[i]);
+
+                    //Items that increase in quality with age eg. Aged Brie
                     } else if (items[i].quality < 50){
                         items[i].quality = items[i].quality + 1;
                     }
                 }
 
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn -= 1;
 
-                if (items[i].sellIn < 0) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
             }
         }
     }
