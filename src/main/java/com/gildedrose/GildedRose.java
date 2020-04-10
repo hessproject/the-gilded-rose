432f5e74e01539ee88aa1c307f8ea90b8f22c64b
Nick Hess <hessproject@gmail.com>
2020-04-10 15:23:57 -0700

    Create list of non-degradable items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,11 @@
 package com.gildedrose;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class GildedRose {
     Item[] items;
+    ArrayList<String> nonDegradables;
 
     public GildedRose() {
         this.items = new Item[] { //
@@ -12,32 +16,27 @@ class GildedRose {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20), //
                 new Item("Conjured Mana Cake", 3, 6) //
         };
+
+        this.nonDegradables = new ArrayList<String>(List.of("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"));
     }
 
     public void updateQuality(int numberOfDays) {
         for(int days = 0; days < numberOfDays; days++){
             for (int i = 0; i < items.length; i++) {
-                if (!items[i].name.equals("Aged Brie")
-                        && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!nonDegradables.contains(items[i].name)) {
                     if (items[i].quality > 0) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                            items[i].quality = items[i].quality - 1;
-                        }
+                        items[i].quality = items[i].quality - 1;
                     }
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
 
                         if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].sellIn < 11) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
-                                }
-                            }
-                            //Redundant
-                            if (items[i].sellIn < 6) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1;
+                            if (items[i].quality < 50) {
+                                if(items[i].sellIn < 6){
+                                    items[i].quality += 2;
+                                } else if(items[i].sellIn < 11) {
+                                    items[i].quality += 1;
                                 }
                             }
                         }
