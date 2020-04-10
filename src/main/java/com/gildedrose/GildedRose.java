272736a13233bc11dc2b4135098d788e118e75e7
Nick Hess <hessproject@gmail.com>
2020-04-10 15:10:47 -0700

    Add unit test, update method signature to accept number of days

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,53 +14,56 @@ class GildedRose {
         };
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+    public void updateQuality(int numberOfDays) {
+        for(int days = 0; days < numberOfDays; days++){
+            for (int i = 0; i < items.length; i++) {
+                if (!items[i].name.equals("Aged Brie")
+                        && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].quality > 0) {
+                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            items[i].quality = items[i].quality - 1;
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
+
+                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (items[i].sellIn < 11) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1;
+                                }
+                            }
+                            //Redundant
+                            if (items[i].sellIn < 6) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    items[i].sellIn = items[i].sellIn - 1;
+                }
+
+                //Too many nested ifs, redundant name check
+                if (items[i].sellIn < 0) {
+                    if (!items[i].name.equals("Aged Brie")) {
+                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (items[i].quality > 0) {
+                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                    items[i].quality = items[i].quality - 1;
+                                }
+                            }
+                        } else {
+                            items[i].quality = items[i].quality - items[i].quality;
+                        }
+                    } else {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1;
+                        }
                     }
                 }
             }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,10 +7,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void testUpgradeQualityOneDay() {
         GildedRose app = new GildedRose();
-        app.updateQuality();
+
         assertEquals("+5 Dexterity Vest", app.items[0].name);
+        assertEquals("Aged Brie", app.items[1].name);
+        assertEquals("Elixir of the Mongoose", app.items[2].name);
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[3].name);
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[4].name);
+        assertEquals("Conjured Mana Cake", app.items[5].name);
+
+        app.updateQuality(1);
+
+        assertEquals(app.items[0].quality, 19);
+        assertEquals(app.items[1].quality, 1);
+        assertEquals(app.items[2].quality, 6);
+        assertEquals(app.items[3].quality, 80);
+        assertEquals(app.items[4].quality, 21);
     }
+
+
 
 }
