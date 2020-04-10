c6c0f1f7b23fc39ea83d07681817bdfa5e628777
Nick Hess <hessproject@gmail.com>
2020-04-10 16:00:02 -0700

    Add loop break for legendaries, create own method for concert tickets

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -6,6 +6,7 @@ import java.util.List;
 class GildedRose {
     Item[] items;
     ArrayList<String> nonDegradables;
+    ArrayList<String> legendaryItems;
 
     public GildedRose() {
         this.items = new Item[] { //
@@ -17,12 +18,18 @@ class GildedRose {
                 new Item("Conjured Mana Cake", 3, 6) //
         };
 
-        this.nonDegradables = new ArrayList<String>(List.of("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"));
+        this.nonDegradables = new ArrayList<String>(List.of("Aged Brie", "Backstage passes to a TAFKAL80ETC concert"));
+        this.legendaryItems = new ArrayList<String>(List.of("Sulfuras, Hand of Ragnaros"));
     }
 
     public void updateQuality(int numberOfDays) {
         for(int days = 0; days < numberOfDays; days++){
             for (int i = 0; i < items.length; i++) {
+
+                if(legendaryItems.contains(items[i].name)){
+                    continue;
+                }
+
                 if (!nonDegradables.contains(items[i].name)) {
                     if (items[i].quality > 0) {
                         if(items[i].sellIn >= 0){
@@ -33,24 +40,14 @@ class GildedRose {
                     }
                 } else {
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if(items[i].sellIn < 0){
-                            items[i].quality = 0;
-                        } else if(items[i].sellIn < 6){
-                            items[i].quality += 3;
-                        } else if(items[i].sellIn < 11) {
-                            items[i].quality += 2;
-                        } else {
-                            items[i].quality += 1;
-                        }
+                        items[i] = adjustConcertTickets(items[i]);
                     } else if (items[i].quality < 50){
                         items[i].quality = items[i].quality + 1;
                     }
                 }
 
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                    items[i].sellIn = items[i].sellIn - 1;
-                }
-                
+                items[i].sellIn = items[i].sellIn - 1;
+
                 if (items[i].sellIn < 0) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
@@ -58,6 +55,20 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private Item adjustConcertTickets(Item concertTicket){
+        if(concertTicket.sellIn < 0){
+            concertTicket.quality = 0;
+        } else if(concertTicket.sellIn < 6){
+            concertTicket.quality += 3;
+        } else if(concertTicket.sellIn < 11) {
+            concertTicket.quality += 2;
+        } else {
+            concertTicket.quality += 1;
+        }
+
+        return concertTicket;
     }
 }
 
