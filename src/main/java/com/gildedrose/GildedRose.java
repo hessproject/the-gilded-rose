42130adfe53d1c285c7e9760a5f472ae9132cf4b
Nick Hess <hessproject@gmail.com>
2020-04-10 15:37:31 -0700

    Simplify quality adjustments

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,21 +25,25 @@ class GildedRose {
             for (int i = 0; i < items.length; i++) {
                 if (!nonDegradables.contains(items[i].name)) {
                     if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
+                        if(items[i].sellIn >= 0){
+                            items[i].quality -= 1;
+                        } else {
+                            items[i].quality -= 2;
+                        }
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-
-                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].quality < 50) {
-                                if(items[i].sellIn < 6){
-                                    items[i].quality += 2;
-                                } else if(items[i].sellIn < 11) {
-                                    items[i].quality += 1;
-                                }
-                            }
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if(items[i].sellIn < 0){
+                            items[i].quality = 0;
+                        } else if(items[i].sellIn < 6){
+                            items[i].quality += 3;
+                        } else if(items[i].sellIn < 11) {
+                            items[i].quality += 2;
+                        } else {
+                            items[i].quality += 1;
                         }
+                    } else if (items[i].quality < 50){
+                        items[i].quality = items[i].quality + 1;
                     }
                 }
 
@@ -49,23 +53,12 @@ class GildedRose {
 
                 //Too many nested ifs, redundant name check
                 if (items[i].sellIn < 0) {
-                    if (!items[i].name.equals("Aged Brie")) {
-                        if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (items[i].quality > 0) {
-                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    items[i].quality = items[i].quality - 1;
-                                }
-                            }
-                        } else {
-                            items[i].quality = items[i].quality - items[i].quality;
-                        }
-                    } else {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
     }
 }
+
